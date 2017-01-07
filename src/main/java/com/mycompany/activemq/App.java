ffaffb1d939f4b4e86091be62638cb400002b0a5
Colin But <colinbut@users.noreply.github.com>
2017-01-07 10:31:16 +0000

    tidied up maven template

--- src/main/java/com/mycompany/activemq/App.java
+++ src/main/java/com/mycompany/activemq/App.java
@@ -1,13 +1,9 @@
 package com.mycompany.activemq;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("App");
     }
+
 }
