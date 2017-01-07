144a4cefa58bc584b101d7522cd921630710b02b
Colin But <colinbut@users.noreply.github.com>
2017-01-07 21:15:17 +0000

    updated corrected copyright

--- src/main/java/com/mycompany/activemq/util/PropertyLoader.java
+++ src/main/java/com/mycompany/activemq/util/PropertyLoader.java
@@ -1,6 +1,6 @@
 /*
  * |-------------------------------------------------
- * | Copyright © 2016 Colin But. All rights reserved. 
+ * | Copyright © 2017 Colin But. All rights reserved.
  * |-------------------------------------------------
  */
 package com.mycompany.activemq.util;
